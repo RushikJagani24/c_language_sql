5ea5e14b48d61d9c9df8705c60fc44b6e1122b3a
RushikJagani24 <rushikjagani24@gmail.com>
2023-08-04 17:47:27 +0530

    done

---
+++ Kajal Mehta/C++/Modul 4(c++)/inline5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+inline void get()
+{
+	int n;
+	cout<<"Enter Number:";
+	cin>>n;
+	cout<<"Sqaure:"<<n*n<<endl;
+	cout<<"Cube:"<<n*n*n;
+}
+main()
+{
+	get();
+}
